c407e38ede167c04d17e65798415ecc279da6770
Abhishek V Joshi <abhivjoshi.aj@gmail.com>
2017-05-30 00:13:15 +0530

    Commenting of forms.py

--- pokemon/forms.py
+++ pokemon/forms.py
@@ -1,7 +1,9 @@
+# Extending the User model of django to make the authentication system
 from django.contrib.auth.models import User
 from django import forms
 from .models import *
 
+# This form is used to take as input the team details while registering into the game.
 class TeamForm(forms.Form):
 	teamname = forms.CharField(max_length=50,label='Team Name (Username):')
 	password = forms.CharField(widget=forms.PasswordInput(),max_length=50)
@@ -14,6 +16,7 @@ class TeamForm(forms.Form):
 	idno1 = forms.CharField(max_length=20)
 	idno2 = forms.CharField(max_length=20)
 
+# This form is used to authenticate an existing (registered) team and log it in to the game.
 class LoginForm(forms.Form):
 	teamname = forms.CharField(max_length = 50)
 	password = forms.CharField(widget=forms.PasswordInput())
